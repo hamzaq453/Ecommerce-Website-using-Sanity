549a9de68500e103d0bc40a9b8b0d461225d4c97
Hamza <qhamza4532@gmail.com>
2024-03-05 13:32:43 +0500

    up

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,19 +1,19 @@
-import React from 'react'
+import React from 'react';
 
 function Footer() {
   return (
-    <footer className="flex lg:flex-row flex-col gap-5 justify-between px-8 py-6 border-t border-black mt-20">
-      <p className="leading-7 [&:not(:first-child)]:mt-0">
-      Copyright © 2024 NextCommerce
-        </p>
-        <p className="leading-7 [&:not(:first-child)]:mt-0">
+    <footer className="lg:flex lg:flex-row flex-col gap-5 justify-between px-8 py-6 border-t border-black mt-20">
+      <p className="leading-7 lg:mt-0">
+        Copyright © 2024 NextCommerce
+      </p>
+      <p className="leading-7 lg:mt-0">
         Design by. <span className='font-bold'> Hamza Qureshi</span>
-        </p>
-        <p className="leading-7 [&:not(:first-child)]:mt-0">
+      </p>
+      <p className="leading-7 lg:mt-0">
         Code by. <span className='font-bold'>Hamza Qureshi</span> 
-        </p>
-  </footer>
-  )
+      </p>
+    </footer>
+  );
 }
 
 export default Footer;
